3022ca6480bfa51da58acc8534b51d4e34cbd7b8
wiktor.dufresne1 <wiktor.dufresne1@CH-21>
2024-10-08 17:56:11 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Przejscie do \nnowej lini");
+Console.WriteLine("Tabulacja \tpozioma");
+Console.WriteLine("Tabulacja \npionowa");
+Console.WriteLine("Alarm! \a");
+Console.WriteLine("Pojedynczy apostrof \'");
+Console.WriteLine("Pojedynczy cudzyslow \"");
+Console.WriteLine("Znak pusty \0");
